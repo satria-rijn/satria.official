0703055d45dcd5dee45586ad875c956f618881fa
satria-rijn <sateia776@gmail.com>
2025-02-19 22:15:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// toogle class ative
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', (e) => {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
